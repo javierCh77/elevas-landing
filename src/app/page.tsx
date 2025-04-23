da291c9e0805268fc03f9dc8f9f949f54b99f2a6
javierCh77 <javierchavarria21@gmail.com>
2025-04-23 13:00:50 -0300

    update

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -158,7 +158,7 @@ export default function Home() {
           </motion.div>
 
           <motion.div
-            className="grid gap-6 sm:grid-cols-2 lg:grid-cols-3"
+            className="grid gap-6 sm:grid-cols-2 lg:grid-cols-3 "
             initial="hidden"
             whileInView="visible"
             viewport={{ once: true }}
